e311854b65157d94906ed08f878c835cf6b91de4
vikas-viki <viki57394@gmail.com>
2025-07-22 22:07:28 +0530

    day 6

--- Day6.java
+++ Day6.java
@@ -2,7 +2,10 @@ import java.util.Arrays;
 
 public class Day6 {
     public static void main(String[] args) {
-
+        int arr[] = { 1, 2, 4, 7, 9 };
+        // int ans = binarySearch(arr, 7);
+        int ans = binaryRecursiveSearch(arr, 70, 0, 4);
+        System.out.println(ans);
     }
 
     public int[] productExceptSelf(int[] nums) {
@@ -20,5 +23,36 @@ public class Day6 {
         }
 
         return ans;
+    }
+
+    public static int binarySearch(int nums[], int ele) { // logn
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + ((end - start) / 2);
+
+            if (ele < nums[mid]) {
+                end = mid - 1;
+            } else if (ele > nums[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static int binaryRecursiveSearch(int nums[], int ele, int start, int end) {
+        int mid = start + ((end - start) / 2);
+        if (start > end)
+            return -1;
+        if (ele < nums[mid]) {
+            return binaryRecursiveSearch(nums, ele, start, mid - 1);
+        } else if (ele > nums[mid]) {
+            return binaryRecursiveSearch(nums, ele, mid + 1, end);
+        } else {
+            return mid;
+        }
     }
 }
